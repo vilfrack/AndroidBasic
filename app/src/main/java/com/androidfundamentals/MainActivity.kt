ddc798907bff8f6e9f886d83ab23655cedfdad0a
jvillarroelc <jvillarroel@megatlon.com.ar>
2020-07-26 21:44:43 -0300

    Show information in log to android studio

--- app/src/main/java/com/androidfundamentals/MainActivity.kt
+++ app/src/main/java/com/androidfundamentals/MainActivity.kt
@@ -11,6 +11,19 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val list = listOf(5,2,8,6,3)
+        sortList(list)
+        Log.d("MainActivity","Hello this is my firts log msg ")
+        println(list)
+    }
+    private fun sortList(list:List<Int>){
+        for (i in 0..list.size -1){
+            for (j in 0..list.size -2){
+                if (list[j] > list[j+1]){
+                    Collections.swap(list,j,j+1)
+                }
+            }
+        }
     }
 
 
