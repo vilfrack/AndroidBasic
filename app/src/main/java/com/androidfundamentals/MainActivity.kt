f5eb7bdf3c3bb7cc92756b691a255f240cedc0d9
jvillarroelc <jvillarroel@megatlon.com.ar>
2020-07-26 21:40:39 -0300

    first commit

---
+++ app/src/main/java/com/androidfundamentals/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.androidfundamentals
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import java.util.*
+import kotlin.math.log
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+
+
+}
