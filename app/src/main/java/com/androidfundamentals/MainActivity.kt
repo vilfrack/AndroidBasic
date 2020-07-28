e793ce5cf8c1e4070a896d70e44552b640030c9b
jvillarroelc <jvillarroel@megatlon.com.ar>
2020-07-28 19:55:43 -0300

    Created layout with one button and textview, where add one value for click in button

--- app/src/main/java/com/androidfundamentals/MainActivity.kt
+++ app/src/main/java/com/androidfundamentals/MainActivity.kt
@@ -3,6 +3,7 @@ package com.androidfundamentals
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
 import java.util.*
 import kotlin.math.log
 
@@ -10,7 +11,11 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        var count =0
+        btnCount.setOnClickListener {
+            count++
+            tvCount.text = "Let's count together: $count"
+        }
     }
 
 
