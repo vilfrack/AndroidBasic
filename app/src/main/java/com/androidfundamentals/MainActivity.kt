bddaa12cafac2b1e3be2cc08c56298a855c2fdbe
jvillarroelc <jvillarroel@megatlon.com.ar>
2020-07-27 21:28:48 -0300

    Linear Layout

--- app/src/main/java/com/androidfundamentals/MainActivity.kt
+++ app/src/main/java/com/androidfundamentals/MainActivity.kt
@@ -10,21 +10,5 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        val list = listOf(5,2,8,6,3)
-        sortList(list)
-        Log.d("MainActivity","Hello this is my firts log msg ")
-        println(list)
     }
-    private fun sortList(list:List<Int>){
-        for (i in 0..list.size -1){
-            for (j in 0..list.size -2){
-                if (list[j] > list[j+1]){
-                    Collections.swap(list,j,j+1)
-                }
-            }
-        }
-    }
-
-
 }
