a7396270d2b1645604f13fbb667f790a671bc38c
jvillarroelc <jvillarroel@megatlon.com.ar>
2020-07-30 20:21:28 -0300

    Toast custimice

--- app/src/main/java/com/androidfundamentals/MainActivity.kt
+++ app/src/main/java/com/androidfundamentals/MainActivity.kt
@@ -3,6 +3,9 @@ package com.androidfundamentals
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.custom_toast.*
 import java.util.*
 import kotlin.math.log
 
@@ -11,7 +14,13 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        BtnShowToast.setOnClickListener {
+           Toast(this).apply {
+               duration = Toast.LENGTH_LONG
+               view = layoutInflater.inflate(R.layout.custom_toast,clToast)
+               //el segundo parametro es el id del layaout constrains del custom layout
+               show()
+           }
+        }
     }
-
-
 }
