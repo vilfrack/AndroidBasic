3a94dbc621aa7c6e612d2df2730d1b499ad23452
jvillarroelc <jvillarroel@megatlon.com.ar>
2020-07-31 20:50:51 -0300

    Implemente Toolbarmenu

--- app/src/main/java/com/androidfundamentals/MainActivity.kt
+++ app/src/main/java/com/androidfundamentals/MainActivity.kt
@@ -3,6 +3,9 @@ package com.androidfundamentals
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 import java.util.*
 import kotlin.math.log
 
@@ -13,5 +16,20 @@ class MainActivity : AppCompatActivity() {
 
     }
 
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.app_bar_menu,menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.miAddContact -> Toast.makeText(this,"You clicked on Add Contact", Toast.LENGTH_SHORT).show()
+            R.id.miFavorites -> Toast.makeText(this,"You clicked on Favorites", Toast.LENGTH_SHORT).show()
+            R.id.miSettings -> Toast.makeText(this,"You clicked on Settings", Toast.LENGTH_SHORT).show()
+            R.id.miClose -> finish()
+            R.id.miFeedback -> Toast.makeText(this,"You clicked on Feedback", Toast.LENGTH_SHORT).show()
+        }
+        return true
+    }
 
 }
