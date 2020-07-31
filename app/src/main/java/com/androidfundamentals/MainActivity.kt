25a6571b20b02c062e8552f1ff271c7524ef62bf
jvillarroelc <jvillarroel@megatlon.com.ar>
2020-07-31 18:54:00 -0300

    Create implicit intents where get images from our gallery

--- app/src/main/java/com/androidfundamentals/MainActivity.kt
+++ app/src/main/java/com/androidfundamentals/MainActivity.kt
@@ -1,8 +1,11 @@
 package com.androidfundamentals
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
 import java.util.*
 import kotlin.math.log
 
@@ -11,7 +14,21 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        btnTakePhoto.setOnClickListener {
+            Intent(Intent.ACTION_GET_CONTENT).also {
+                it.type = "image/*"//acepta cualquier formato
+                startActivityForResult(it,0)
+            }
+        }
+
     }
 
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode== Activity.RESULT_OK && requestCode == 0){
+            val uri = data?.data
+            ivPhoto.setImageURI(uri)
+        }
+    }
 
 }
