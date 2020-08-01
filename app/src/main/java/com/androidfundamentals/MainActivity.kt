3002d282ec53eb1c30419f300c906762818badcd
jvillarroelc <jvillarroel@megatlon.com.ar>
2020-08-01 17:17:50 -0300

    Implemente Fragment Layout

--- app/src/main/java/com/androidfundamentals/MainActivity.kt
+++ app/src/main/java/com/androidfundamentals/MainActivity.kt
@@ -2,15 +2,35 @@ package com.androidfundamentals
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import java.util.*
-import kotlin.math.log
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val firtsFragment = FirtsFragment()
+        val SecondFragment = SecondFragment()
+
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.flFragment, firtsFragment)
+            commit()
+        }
+
+        btnFragment1.setOnClickListener {
+            supportFragmentManager.beginTransaction().apply {
+                replace(R.id.flFragment, firtsFragment)
+                addToBackStack(null)//PERMITE NO CERRAR LA APLICACION CUANDO SE HACE CLICK EN LA FLECHA DE ATRAS
+                commit()
+            }
+        }
+        btnFragment2.setOnClickListener {
+            supportFragmentManager.beginTransaction().apply {
+                replace(R.id.flFragment, SecondFragment)
+                addToBackStack(null)//PERMITE NO CERRAR LA APLICACION CUANDO SE HACE CLICK EN LA FLECHA DE ATRAS
+                commit()
+            }
+        }
     }
 
 
