6d4b3ce23ba49accc04fd643c090f3d133ffa7f1
Juan villarroel <vilfrack21@gmail.com>
2020-08-06 20:43:05 -0300

    Notifications

--- app/src/main/java/com/androidfundamentals/MainActivity.kt
+++ app/src/main/java/com/androidfundamentals/MainActivity.kt
@@ -1,16 +1,62 @@
 package com.androidfundamentals
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.app.TaskStackBuilder
+import android.content.Context
+import android.content.Intent
+import android.graphics.Color
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+import kotlinx.android.synthetic.main.activity_main.*
 import java.util.*
 import kotlin.math.log
 
 class MainActivity : AppCompatActivity() {
+
+    val CHANNE_ID = "channelID"
+    val CHANNEL_NAME = "channelName"
+    val NOTIFICATION_ID = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        createNotificationChannel()
+        //ESTE CODIGO PERMITE QUE CUANDO LLEGUE LA NOTIFICACION, AL HACERLE CLICK NOS LLEVE NUEVAMENTE A LA APP
+        val intent = Intent(this, MainActivity::class.java)
+        val pendingIntent= TaskStackBuilder.create(this).run {
+            addNextIntentWithParentStack(intent)
+            getPendingIntent(0,PendingIntent.FLAG_UPDATE_CURRENT)
+        }
+        //-------------------------------------------------------------------------------------------------------
+        val notification = NotificationCompat.Builder(this,CHANNE_ID)
+            .setContentTitle("Awesome notification")
+            .setContentText("This is the content text")
+            .setSmallIcon(R.drawable.ic_start)
+            .setPriority(NotificationCompat.PRIORITY_HIGH)
+            .setContentIntent(pendingIntent)
+            .build()
 
+        val notificationMnager = NotificationManagerCompat.from(this)
+        btnShowNotification.setOnClickListener {
+            notificationMnager.notify(NOTIFICATION_ID, notification)
+        }
+    }
+    fun createNotificationChannel(){
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+            val channel = NotificationChannel(CHANNE_ID,CHANNEL_NAME,
+                NotificationManager.IMPORTANCE_DEFAULT).apply {
+                lightColor = Color.GREEN
+                enableLights(true)
+            }
+            val manager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            manager.createNotificationChannel(channel)
+        }
     }
 
 
