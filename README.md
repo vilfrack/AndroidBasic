1a3c374cde8e19f2eaacccae28c04c91890ea4ab
jvillarroelc <jvillarroel@megatlon.com.ar>
2020-08-05 22:32:01 -0300

    Slidable Menu

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # AndroidBasic
+
+#se necesita instalar la siguiente dependencia : implementation 'com.google.android.material:material:1.3.0-alpha02'
+esta dependencia es la de material
--- app/src/main/java/com/androidfundamentals/MainActivity.kt
+++ app/src/main/java/com/androidfundamentals/MainActivity.kt
@@ -3,15 +3,50 @@ package com.androidfundamentals
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
+import kotlinx.android.synthetic.main.activity_main.*
 import java.util.*
 import kotlin.math.log
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var toggle: ActionBarDrawerToggle
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        //hay que crear las variables del xml de string
+        toggle = ActionBarDrawerToggle(this, drawerLayout, R.string.open, R.string.close)
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
 
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        navView.setNavigationItemSelectedListener {
+            when(it.itemId){
+                R.id.miItem1 ->{
+                    Toast.makeText(applicationContext,"Clicked Item 1", Toast.LENGTH_SHORT).show()
+                    drawerLayout.closeDrawers()
+                }
+                R.id.miItem2 ->{
+                    Toast.makeText(applicationContext,"Clicked Item 2", Toast.LENGTH_SHORT).show()
+                    drawerLayout.closeDrawers()
+                }
+                R.id.miItem3 -> {
+                    Toast.makeText(applicationContext,"Clicked Item 3", Toast.LENGTH_SHORT).show()
+                    drawerLayout.closeDrawers()
+                }
+            }
+            true
+        }
     }
 
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (toggle.onOptionsItemSelected(item)){
+            return true
+        }
+        return super.onOptionsItemSelected(item)
+    }
 
 }
