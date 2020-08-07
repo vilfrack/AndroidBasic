611de1066f342b6b578dba79e0c9e41fc424afd8
Juan villarroel <vilfrack21@gmail.com>
2020-08-06 21:36:37 -0300

    Intent Service

--- app/src/main/java/com/androidfundamentals/MainActivity.kt
+++ app/src/main/java/com/androidfundamentals/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.androidfundamentals
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
 import java.util.*
 import kotlin.math.log
 
@@ -11,6 +13,17 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        btnStartService.setOnClickListener {
+            Intent(this, MyIntentService::class.java).also {
+                startService(it)
+                tvServiceInfo.text = "Service running"
+            }
+        }
+
+        btnStopService.setOnClickListener {
+            MyIntentService.stopService()
+            tvServiceInfo.text = "Service stopped"
+        }
     }
 
 
