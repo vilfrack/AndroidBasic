62b3a592fab4465215c502b0a3e3159a8ad8d3f7
Juan villarroel <vilfrack21@gmail.com>
2020-08-13 19:42:54 -0300

    Broadcast-Receivers

---
+++ app/src/main/java/com/androidfundamentals/AirplaneModeChangedReceiver.kt
@@ -0,0 +1,18 @@
+package com.androidfundamentals
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.widget.Toast
+
+class AirplaneModeChangedReceiver : BroadcastReceiver() {
+
+    override fun onReceive(context: Context?, intent: Intent?) {
+        val isAirplaneModeEnable = intent?.getBooleanExtra("state",false) ?: return
+        if (isAirplaneModeEnable){
+            Toast.makeText(context, "Airplane Mode enabled", Toast.LENGTH_SHORT).show()
+        }else{
+            Toast.makeText(context, "Airplane Mode disiabled", Toast.LENGTH_SHORT).show()
+        }
+    }
+}
--- app/src/main/java/com/androidfundamentals/MainActivity.kt
+++ app/src/main/java/com/androidfundamentals/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.androidfundamentals
 
+import android.content.Intent
+import android.content.IntentFilter
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -7,11 +9,23 @@ import java.util.*
 import kotlin.math.log
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var receiver : AirplaneModeChangedReceiver
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        receiver = AirplaneModeChangedReceiver()
+
+        IntentFilter(Intent.ACTION_AIRPLANE_MODE_CHANGED).also {
+            registerReceiver(AirplaneModeChangedReceiver(), it)
+        }
     }
 
+    override fun onStop() {
+        super.onStop()
+        unregisterReceiver(receiver)
+    }
 
 }
