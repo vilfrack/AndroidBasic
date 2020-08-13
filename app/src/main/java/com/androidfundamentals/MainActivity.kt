af7f1621ef4599b5488de7b1a925fa9563ef36ce
Juan villarroel <vilfrack21@gmail.com>
2020-08-13 19:24:52 -0300

    Drag and drop, puede usarse para hacer un juegos de damas o ajedrez

--- app/src/main/java/com/androidfundamentals/MainActivity.kt
+++ app/src/main/java/com/androidfundamentals/MainActivity.kt
@@ -1,8 +1,16 @@
 package com.androidfundamentals
 
+import android.content.ClipData
+import android.content.ClipDescription
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.DragEvent
+import android.view.View
+import android.view.ViewGroup
+import android.widget.LinearLayout
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 import java.util.*
 import kotlin.math.log
 
@@ -11,7 +19,57 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-    }
+        llTop.setOnDragListener(dragListener)
+        llBotom.setOnDragListener(dragListener)
 
+        dragView.setOnLongClickListener {
+            val clipText = "This is our ClipData text"
+            val item = ClipData.Item(clipText)
+            val mimeType = arrayOf(ClipDescription.MIMETYPE_TEXT_PLAIN)
+            val data = ClipData(clipText, mimeType, item)
+
+            val dragShadowBuilder = View.DragShadowBuilder(it)
+            it.startDragAndDrop(data, dragShadowBuilder, it,0)
+
+            it.visibility = View.INVISIBLE
+            true
+        }
+    }
+    val dragListener = View.OnDragListener { view, event ->
+        when(event.action){
+            DragEvent.ACTION_DRAG_STARTED -> {
+                event.clipDescription.hasMimeType(ClipDescription.MIMETYPE_TEXT_PLAIN)
+            }
+            DragEvent.ACTION_DRAG_ENTERED -> {
+                view.invalidate()
+                true
+            }
+            DragEvent.ACTION_DRAG_LOCATION -> true
+            DragEvent.ACTION_DRAG_EXITED -> {
+                view.invalidate()
+                true
+            }
+            DragEvent.ACTION_DROP -> {
+                val item= event.clipData.getItemAt(0)
+                val dragData = item.text
+                Toast.makeText(this,dragData,Toast.LENGTH_SHORT).show()
+
+                view.invalidate()
+
+                val v = event.localState as View
+                val owner= v.parent as ViewGroup
+                owner.removeView(v)
+                val destination = view as LinearLayout
+                destination.addView(v)
+                v.visibility = View.VISIBLE
+                true
+            }
+            DragEvent.ACTION_DRAG_ENDED -> {
+                view.invalidate()
+                true
+            }
+            else -> false
+        }
+    }
 
 }
